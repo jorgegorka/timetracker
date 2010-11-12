465131a64dd9602c48e8d00bb8721c3265198127
Jorge Alvarez <jorge@alvareznavarro.es>
2010-11-12 18:07:08 +0100

    Added base template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+
+  def about
+
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,17 @@ module ApplicationHelper
     defined?(@header_title) ? @header_title : t('header.default_title')
   end
 
+  def render_flash_messages
+    flash.collect do |type, errors|
+      next if errors.empty?
+
+      content_tag :div, :class=> "notice #{type}" do
+        content_tag :ul do
+          errors.each { |error| content_tag(:li, error) }
+        end
+      end
+    end
+  end
+  
+
 end
